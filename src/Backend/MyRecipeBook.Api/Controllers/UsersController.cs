b9234e6139e08653eaad5624e39449aedacdd7f6
Arthur David Cardoso de Souza Luna <arthurl@abcbrasil.com.br>
2026-05-28 15:28:38 -0300

    feat adding endpoint for register user account

---
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Api.Controllers;
+
+[Route("api/[controller]")] // define a rota para este controlador, por exemplo: api/users
+[ApiController]
+public class UsersController : ControllerBase
+{
+
+    [HttpPost] // identifica que é um endpoint do tipo post
+    public IActionResult Register([FromBody] RequestRegisterUserAccountJson request) // endpoint para registrar um novo user, recebe os dados do usuário no corpo da requisição
+    {
+        return Created(); // retorna status code 201
+    }
+}
--- src/Backend/MyRecipeBook.Api/Program.cs
+++ src/Backend/MyRecipeBook.Api/Program.cs
@@ -5,16 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // adicionando o SwaggerGen para gerar a documentação do Swagger
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // adicionando o middleware do Swagger para servir a documentação gerada
+    app.UseSwaggerUI(); // adicionando o middleware do Swagger UI para fornecer uma interface de usuário para a documentação do Swagger
 }
 
 app.UseHttpsRedirection();
